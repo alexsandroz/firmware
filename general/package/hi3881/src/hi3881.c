f4a82046ac1497135de78080f45842b9169c2703
alexsandroz <alexsandroz@gmail.com>
2023-11-01 17:33:17 -0300

    Converto to kernel module

--- general/package/hi3881/src/hi3881.c
+++ general/package/hi3881/src/hi3881.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	printf("Hellow world!");
+//	printf("Hellow world!!");
 	return 0;
 }
--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,5 @@
 #BOARD=hi3516ev300_ultimate  make br-vtund-openipc-{dirclean,rebuild}
 
 #BOARD=hi3516ev300_ultimate  make br-autonight-rebuild
+rm -rf /home/alex/firmware/output/build/hi3881
 BOARD=hi3516ev300_ultimate  make br-hi3881-rebuild
