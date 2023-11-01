040bd1a80e6c72f8acf1ff13c086f99840a424f8
alexsandroz <alexsandroz@gmail.com>
2023-11-01 16:33:30 -0300

    Criacao modulo basico

---
+++ rebuild.sh
@@ -0,0 +1,6 @@
+#BOARD=hi3516ev300_ultimate  make br-linux-{dirclean,rebuild}
+#BOARD=hi3516ev300_ultimate  make br-rootfs-{squashfs,tar}
+#BOARD=hi3516ev300_ultimate  make br-vtund-openipc-{dirclean,rebuild}
+
+#BOARD=hi3516ev300_ultimate  make br-autonight-rebuild
+BOARD=hi3516ev300_ultimate  make br-hi3881-rebuild
